f64ffcdf8f07304406d28a8bf9393198fdc44abe
Rahul kabburi <rahul.rkabburi@gmail.com>
2025-01-10 14:08:50 +0530

    First DOM project

---
+++ script.js
@@ -0,0 +1,9 @@
+let target = document.getElementById("myDiv");
+target.style.backgroundColor = "yellow";
+target.style.margin = "20px";
+target.style.padding = "10px";
+target.style.fontSize = "18px";
+target.style.fontWeight = "bold";
+target.style.height = "200px";
+target.style.width = "300px";
+target.style.color = "blue";
